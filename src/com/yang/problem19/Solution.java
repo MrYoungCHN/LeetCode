ef793e3b08fa4bbb734e89e41f9fc760bc33c11c
Daze <yy349748880@gmail.com>
2021-01-19 23:13:21 +0800

    19.删除链表的倒数第N个结点(中等)

---
+++ src/com/yang/problem19/Solution.java
@@ -0,0 +1,54 @@
+package com.yang.problem19;
+
+import com.yang.common.ListNode;
+
+/**
+ * 19. 删除链表的倒数第N个结点(中等)
+ * 给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
+ * 进阶：你能尝试使用一趟扫描实现吗？
+ *
+ * 示例 1：
+ * 输入：head = [1,2,3,4,5], n = 2
+ * 输出：[1,2,3,5]
+ *
+ * 示例 2：
+ * 输入：head = [1], n = 1
+ * 输出：[]
+ *
+ * 示例 3：
+ * 输入：head = [1,2], n = 1
+ * 输出：[1]
+ *
+ *
+ * 提示：
+ * 链表中结点的数目为 sz
+ * 1 <= sz <= 30
+ * 0 <= Node.val <= 100
+ * 1 <= n <= sz
+ *
+ * @author Daze
+ * @date 2021-01-19
+ */
+public class Solution {
+    /**
+     * 双指针(快慢指针)法
+     *
+     * @param head 头节点
+     * @param n 要删除的节点倒数位置
+     * @return 删除之后的链表头节点
+     */
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0, head);
+        ListNode first = head;
+        ListNode second = dummy;
+        for (int i = 0; i < n; i++) {
+            first = first.next;
+        }
+        while (first != null) {
+            first = first.next;
+            second = second.next;
+        }
+        second.next = second.next.next;
+        return dummy.next;
+    }
+}
